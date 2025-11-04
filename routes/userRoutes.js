bf347b3ced78574ca6580cef9f4b3df39143aeb8
arjunasn476 <arjuna_notanubun_33rpl@student.smktelkom-mlg.sch.id>
2025-11-04 14:16:54 +0700

    Delete routes directory

--- routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const { verifyToken } = require('../middleware/authMiddleware');
-const { createUser, updateUser, getUser } = require('../controllers/userController');
-
-router.post('/', createUser);
-router.put('/:id', verifyToken, updateUser);
-router.get('/:id', verifyToken, getUser);
-
-module.exports = router;
